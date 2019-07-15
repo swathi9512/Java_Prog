e916df19c8a2c2e561e11f76b703ee3f8d5da832
Swathi_Su <Swathi_Su@DESKTOP-HPRS7KQ>
2019-07-15 19:32:45 +0200

    Boss

---
+++ Konstruktor/src/Boss.java
@@ -0,0 +1,17 @@
+
+public class Boss 
+{
+	private String bossName;
+	
+	//Konstruktor
+	public Boss(String name)
+	{
+		bossName = name;
+	}
+	
+	//Methode
+	public void schreibBossName()
+	{
+		System.out.println(bossName + " ist dein Boss");
+	}
+}
---
+++ Konstruktor/src/TestBoss.java
@@ -0,0 +1,15 @@
+
+public class TestBoss 
+{
+	public static void main(String[] args)
+	{
+		Boss objekt1 = new Boss("Wer");
+		Boss objekt2 = new Boss("Informatik");
+		
+		//Aufruf der Methode von Boss.java// 
+		objekt1.schreibBossName();
+		objekt2.schreibBossName();
+	}
+
+	
+}
